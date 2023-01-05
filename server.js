9bf5870162f0676aec3304c2203f097d46bdf4ee
arnaldobatista <arnaldo.carpi@icloud.com>
2023-01-05 15:51:33 -0300

    ativando mario bot para slaney.

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const client = new Client({
     authStrategy: new LocalAuth()
 });
- 
+
 
 client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+    qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
@@ -15,4 +15,17 @@ client.on('ready', () => {
 });
 
 client.initialize();
- 
+
+const nomes = {
+    arnaldo: '5522988223236@c.us',
+    slaney: '5522996015586@c.us',
+}
+
+client.on('message', message => {
+
+    console.log(message)
+
+    if (message.author === nomes.slaney) {
+        message.reply('Te levantei até onde pude.')
+    }
+});
