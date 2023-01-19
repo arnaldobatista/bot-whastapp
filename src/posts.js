23b18f21cd9e0f78ebe5c8b24832a3015287f969
arnaldobatista <arnaldo.carpi@icloud.com>
2023-01-19 11:56:15 -0300

    feat: Download Videos and Images from Twitter implemented

--- src/posts.js
+++ src/posts.js
@@ -4,13 +4,16 @@ module.exports = class Posts {
     async download(message) {
         if (message.body.startsWith('/baixar')) {
             let url = message.body.split(' ')[1].split('/')[2] === 'music.youtube.com' ? `https://www.youtube.com/${message.body.split(' ')[1].split('/')[3]}` : message.body.split(' ')[1]
-            console.log(url)
             const numberImg = message.body.split(' ')[2]
             const urlType = url.split('/')
 
             const objRetun = await urlDownload(url)
 
             //checks
+            if (objRetun.code === 102) {
+                message.reply('A conversão falhou! Tente novamente ou troque o link!')
+                return
+            }
             //dowload YT
             if (urlType[2] === 'www.youtube.com') {
                 const link = objRetun.url[1].url
@@ -19,27 +22,15 @@ module.exports = class Posts {
             }
             //dowload twitter
             if (urlType[2] === 'twitter.com') {
-                return
-            }
-            //dowload tiktok 
-            if (urlType[2] === 'www.tiktok.com') {
-                return
-            }
-            //dowload instagram
-            if (urlType[2] === 'www.instagram.com') {
+                console.log(objRetun)
+
                 if (objRetun.url) {
-                    if (urlType[3] === 'stories') {
-                        const link = objRetun.url[1].url
-                        message.reply(link)
-                        return
-                    }
-    
                     const link = objRetun.url[0].url
                     message.reply(link)
                 }
-    
+
                 if (objRetun[0]) {
-                    if (!numberImg){
+                    if (!numberImg) {
                         message.reply(`Para baixar uma postagem com mais de um item use:
     
 */baixar https://www.link.com 1*
@@ -48,10 +39,43 @@ O numero no final corresponde a postagem que você quer baixar.
                         `)
                         return
                     }
-    
                     const numberArray = numberImg - 1
                     const link = objRetun[numberArray].url[0].url
                     message.reply(link)
+                    return
+                }
+                //dowload tiktok 
+                if (urlType[2] === 'www.tiktok.com') {
+                    return
+                }
+                //dowload instagram
+                if (urlType[2] === 'www.instagram.com') {
+                    if (objRetun.url) {
+                        if (urlType[3] === 'stories') {
+                            const link = objRetun.url[1].url
+                            message.reply(link)
+                            return
+                        }
+
+                        const link = objRetun.url[0].url
+                        message.reply(link)
+                    }
+
+                    if (objRetun[0]) {
+                        if (!numberImg) {
+                            message.reply(`Para baixar uma postagem com mais de um item use:
+    
+*/baixar https://www.link.com 1*
+
+O numero no final corresponde a postagem que você quer baixar.
+                        `)
+                            return
+                        }
+
+                        const numberArray = numberImg - 1
+                        const link = objRetun[numberArray].url[0].url
+                        message.reply(link)
+                    }
                 }
             }
         }
