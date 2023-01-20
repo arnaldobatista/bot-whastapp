b26df86f8ff129c27af87cbcc1bf75b55664219c
arnaldobatista <arnaldo.carpi@icloud.com>
2023-01-19 21:24:34 -0300

    feat: Implementing API Link Reducer

---
+++ src/api/convertUrl.js
@@ -0,0 +1,28 @@
+const request = require('request')
+
+async function convertUrl(url) {
+    try {
+        const options = {
+            url: `https://api.tinyurl.com/create?api_token=yCCV9UifhjYEw8Txe9BahR401pt6WLyHZrtPaeRmmx2zI5CMXHYIKKDORn2Q`,
+            headers: {
+                'content-type': 'application/json'
+            },
+            method: 'POST',
+            body: `{"url":"${url}"}`
+        }
+        
+        const response = await new Promise((resolve, reject) => {
+            request(options, (error, res, body) => {
+                const link = JSON.parse(res.body.data.tiny_url)
+                resolve(link)
+            })
+        })
+        return response
+    } catch (error) {
+            return "API em manuntenção."
+    }
+}
+
+const url = 'https://www.youtube.com/watch?v=HgVkG_WjcOM&list=sssRDAMVMSYM-RJwSGQ8'
+convertUrl(url)
+module.exports = convertUrl
